b165a30a2f1fe44d9eb1d2d3d7292f206898f911
muskansindhu <shunyamuskan@gmail.com>
2024-05-21 02:03:10 +0530

    Add Monaco Editor

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,32 @@
+require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.23.0/min/vs' } });
+
+let editor;
+let selected = null;
+
+require(["vs/editor/editor.main"], function() {
+    editor = monaco.editor.create(document.getElementById('markdown-content'), {
+        value: ' ',
+        language: 'markdown',
+        theme: 'vs-dark',
+        minimap: { enabled: false },
+        scrollbar: { vertical: "hidden" },
+        lineNumbers: "off",
+    });
+
+    editor.getModel().onDidChangeContent(() => {
+        updatePreview();
+    });
+});
+
 document.addEventListener("DOMContentLoaded", function() {
-    markdownConverter();
     dragAndDrop();
 });
 
-function markdownConverter() {
-    document.getElementById('markdown-content').addEventListener('input', function() {
-        const markdownContent = document.getElementById('markdown-content');
-        const htmlPreview = document.getElementById('preview-content');
-        const htmlContent = marked.parse(markdownContent.value);
-        htmlPreview.innerHTML = DOMPurify.sanitize(htmlContent, { USE_PROFILES: { html: true } });
-    });
+function updatePreview() {
+    const markdownContent = editor.getValue();
+    const htmlPreview = document.getElementById('preview-content');
+    const htmlContent = marked.parse(markdownContent);
+    htmlPreview.innerHTML = DOMPurify.sanitize(htmlContent, { USE_PROFILES: { html: true } });
 }
 
 function dragAndDrop() {
@@ -50,10 +67,8 @@ function dragAndDrop() {
                     textToAdd = description();
                 }
     
-                markdownContent.value += textToAdd;
-                const htmlContent = marked.parse(markdownContent.value);
-                const htmlPreview = document.getElementById('preview-content');
-                htmlPreview.innerHTML = DOMPurify.sanitize(htmlContent, { USE_PROFILES: { html: true } });
+                editor.setValue(editor.getValue() + textToAdd);
+                updatePreview();
                 selected = null;
             }
         });
